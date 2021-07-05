7f8e2af8e0e78bea3ef6e810c25772e6f2281c39
stesha <stepashechka5@gmail.com>
2021-07-05 20:33:20 +0300

    change function name

--- js/put-photo.js
+++ js/put-photo.js
@@ -9,9 +9,9 @@ fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  const checkFileFormat = FILE_TYPES.some((it) => fileName.endsWith(it));
 
-  if (matches) {
+  if (checkFileFormat) {
     const reader = new FileReader();
 
     reader.addEventListener('load', () => preview.src = reader.result);
